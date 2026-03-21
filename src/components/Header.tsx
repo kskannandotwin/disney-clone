30ccc3175fc10b8cae244be38e05e32782c47f91
Jobulikannan <kannan@jobuli.com>
2026-03-21 19:20:35 +0530

    responsive navbar completed
    on 21.03.2026

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import logo from "./../assets/images/logo.png";
 import {
   HiHome,
@@ -6,17 +7,18 @@ import {
   HiPlayCircle,
   HiTv,
 } from "react-icons/hi2";
-import { HiPlus } from "react-icons/hi";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from "./HeaderItem";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   const menu = [
     { name: "Home", icon: HiHome },
     { name: "Search", icon: HiMagnifyingGlass },
     { name: "Watchlist", icon: HiPlus },
     { name: "Originals", icon: HiStar },
     { name: "Movies", icon: HiPlayCircle },
-    { name: "TV Shows", icon: HiTv },
+    { name: "Series", icon: HiTv },
   ];
 
   return (
@@ -27,9 +29,30 @@ const Header = () => {
           className="w-20 md:w-24 object-cover"
           alt="Disney Logo"
         />
-        {menu.map((item) => (
-          <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
-        ))}
+        <div className="hidden md:flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+        <div className="flex md:hidden gap-8">
+          {menu.map(
+            (item, index) =>
+              index < 3 && <HeaderItem name={""} Icon={item.icon} />,
+          )}
+          <div className="md:hidden" onClick={() => setToggle((prev) => !prev)}>
+            <HeaderItem name={""} Icon={HiDotsVertical} />
+            {toggle ? (
+              <div className="absolute mt-3 bg-[#121212] border border-gray-700 p-3 px-5 py-4">
+                {menu.map(
+                  (item, index) =>
+                    index > 2 && (
+                      <HeaderItem name={item.name} Icon={item.icon} />
+                    ),
+                )}
+              </div>
+            ) : null}
+          </div>
+        </div>
       </div>
       <img
         src="https://randomuser.me/api/portraits/women/1.jpg"
--- src/components/HeaderItem.tsx
+++ src/components/HeaderItem.tsx
@@ -5,9 +5,9 @@ type HeaderItemProps = {
 
 const HeaderItem = ({ name, Icon }: HeaderItemProps) => {
   return (
-    <div className="text-white uppercase flex items-center gap-3 text-[15px] not-first:font-semibold cursor-pointer hover:underline underline-offset-8">
+    <div className="text-white uppercase flex items-center gap-3 text-[15px] not-first:font-semibold cursor-pointer hover:underline underline-offset-8 mb-2">
       <Icon />
-      <h2>{name}</h2>
+      <h2 className="">{name}</h2>
     </div>
   );
 };
