4f675c91b72943d760f4778ea6bf15051a726e0c
Jobulikannan <kannan@jobuli.com>
2026-03-22 21:29:11 +0530

    production house completed
    on 22.03.2026

---
+++ src/components/ProductionHouse.tsx
@@ -0,0 +1,62 @@
+import disney from "./../assets/images/disney.png";
+import marvel from "./../assets/images/marvel.png";
+import pixar from "./../assets/images/pixar.png";
+import starwars from "./../assets/images/starwar.png";
+import natgeo from "./../assets/images/nationalG.png";
+
+import starwarV from "./../assets/videos/star-wars.mp4";
+import disneyV from "./../assets/videos/disney.mp4";
+import marvelV from "./../assets/videos/marvel.mp4";
+import pixarV from "./../assets/videos/pixar.mp4";
+import natgeoV from "./../assets/videos/national-geographic.mp4";
+
+const ProductionHouse = () => {
+  const productionHouseList = [
+    {
+      id: 1,
+      image: disney,
+      video: disneyV,
+    },
+    {
+      id: 2,
+      image: marvel,
+      video: marvelV,
+    },
+    {
+      id: 3,
+      image: pixar,
+      video: pixarV,
+    },
+    {
+      id: 4,
+      image: starwars,
+      video: starwarV,
+    },
+    {
+      id: 5,
+      image: natgeo,
+      video: natgeoV,
+    },
+  ];
+
+  return (
+    <div className="flex gap-2 md:gap-5 p-2 px-5 md:px-16">
+      {productionHouseList.map((item) => (
+        <div
+          key={item.id}
+          className="border border-gray-600 rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer shadow-xl shadow-gray-800"
+        >
+          <video
+            src={item.video}
+            autoPlay
+            loop
+            playsInline
+            className="absolute top-0 rounded-md z-0 opacity-0 hover:opacity-50 transition-opacity duration-300 ease-in-out"
+          />
+          <img src={item.image} className="w-full z-1" />
+        </div>
+      ))}
+    </div>
+  );
+};
+export default ProductionHouse;
